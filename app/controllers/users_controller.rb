b1cf46501733212724fbcc434a68f06389a2177f
NaveedG <nganjani@protonmail.com>
2019-12-30 11:28:51 -0600

    added user controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    user = User.find_or_create_by(username: params[:username])
+    render json: user
+  end
+
 end
