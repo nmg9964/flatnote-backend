4b25d4cb40fff79afb03261705b0b77e51ea22f9
NaveedG <nganjani@protonmail.com>
2019-12-31 10:33:01 -0600

    added index and create note controllers

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,12 @@
 class NotesController < ApplicationController
+  def index
+    notes = Note.all 
+    render json: notes
+  end
+
+  def create
+    note = Note.create(title: params[:title], content: params[:content], user_id: params[user:id])
+    render json: note
+  end
+
 end
