41f6332831eb8b6c1d7fc8e1121ecd33d5ee478b
NaveedG <nganjani@protonmail.com>
2020-01-02 15:38:04 -0600

    fixed users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: users, include: :notes
+    render json: users.to_json(:include => {:notes => {include: :tags}})
   end
 
   def create
     user = User.find_or_create_by(username: params[:username])
-    render json: user, include: :notes
+    render json: user.to_json(:include => {:notes => {include: :tags}})
   end
 
 end
