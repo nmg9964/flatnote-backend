0192bd4059369bcd8887dd0a7b64387160fbafbe
NaveedG <nganjani@protonmail.com>
2020-01-02 13:42:52 -0600

    updated notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,7 @@
 class NotesController < ApplicationController
   def index
     notes = Note.all 
-    render json: notes
+    render json: notes, include: :note_tags
   end
 
   def create
@@ -11,7 +11,7 @@ class NotesController < ApplicationController
 
   def show
     note = Note.find(params[:id])
-    render json: note
+    render json: note, include: :note_tags
   end
 
   def update
