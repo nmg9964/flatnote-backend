1736c54183ee5e7d15fd7cbfa259b9b072ae8384
NaveedG <nganjani@protonmail.com>
2020-01-03 09:29:57 -0600

    reseeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,15 @@ User.create(username: 'Amr')
 
 Note.create(title: 'What is Redux?', content: 'Redux is a small library and design pattern', user_id: 1)
 Note.create(title: 'What is Rails?', content: 'Rails is a heavyweight Ruby framework', user_id: 2)
+Note.create(title: 'Vanilla JS', content: 'Vanilla JS is plain Javascript, no frameworks', user_id: 1)
+Note.create(title: 'Ruby', content: 'Ruby is an OO programming language', user_id: 2)
 
 Tag.create(text: 'Redux')
 Tag.create(text: 'Rails')
+Tag.create(text: 'libraries')
+Tag.create(text: 'frameworks')
 
 NoteTag.create(note_id: 1, tag_id: 1)
+NoteTag.create(note_id: 1, tag_id: 3) 
 NoteTag.create(note_id: 2, tag_id: 2)
+NoteTag.create(note_id: 2, tag_id: 4)
